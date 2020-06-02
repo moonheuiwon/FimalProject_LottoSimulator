0f5153916fe856f48b0304a714f85f854f1932a5
moonheuiwon <wonheuimoon@gmail.com>
2020-06-02 10:37:50 +0900

    보너스 번호 생성 및 출력

--- app/src/main/java/com/example/fimalproject_lottosimulator/MainActivity.java
+++ app/src/main/java/com/example/fimalproject_lottosimulator/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends BaseActivity {
         for (int i = 0; i < winLottoNumArr.length; i ++) {
             winLottoNumArr[i] = 0;
         }
+        bonusNum = 0;
 
         for (int i = 0; i  < winLottoNumArr.length; i ++) {
             while (true) {
@@ -79,10 +80,29 @@ public class MainActivity extends BaseActivity {
         }
         Arrays.sort(winLottoNumArr);
 
+        while (true) {
+
+            int randomNum = (int) (Math.random() * 45 + 1);
+
+            boolean isDuplicatedOk = true;
+
+            for (int num : winLottoNumArr) {
+                if (num == randomNum) {
+                    isDuplicatedOk = false;
+                    break;
+                }
+            }
+            if (isDuplicatedOk) {
+                bonusNum = randomNum;
+                break;
+            }
+        }
+
         for (int i = 0; i < winNumTxts.size(); i ++) {
             int winNum = winLottoNumArr[i];
 
             winNumTxts.get(i).setText(winNum+"");
         }
+        binding.bonusNumTxt.setText(bonusNum+"");
     }
 }
